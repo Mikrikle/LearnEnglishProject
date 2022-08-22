8a66d36973b1b087498efa261e221a1ae35583be
Mihail <miha1502@mail.ru>
2022-08-22 12:33:06 +0300

    Add project files.

---
+++ LearnEnglishApp/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace LearnEnglishApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon_app = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon_app
+            // 
+            this.notifyIcon_app.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon_app.Icon")));
+            this.notifyIcon_app.Text = "LearnEnglish";
+            this.notifyIcon_app.Visible = true;
+            this.notifyIcon_app.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_app_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 153);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NotifyIcon notifyIcon_app;
+    }
+}
---
+++ LearnEnglishApp/Form1.cs
@@ -0,0 +1,25 @@
+namespace LearnEnglishApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetPosition();
+        }
+
+        private void SetPosition()
+        {
+            int boundWidth = Screen.PrimaryScreen.Bounds.Width;
+            int boundHeight = Screen.PrimaryScreen.Bounds.Height;
+            int x = boundWidth - this.Width;
+            int y = boundHeight - this.Height;
+            this.Location = new Point(x - 10, y - 50);
+        }
+
+        private void notifyIcon_app_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
