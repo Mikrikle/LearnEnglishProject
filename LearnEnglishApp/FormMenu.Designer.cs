c61e5081862fb48764fe5d0ab196be82d0db5748
Mihail <mihail.pe@outlook.com>
2022-10-05 23:08:19 +0300

    update menu design

--- LearnEnglishApp/FormMenu.Designer.cs
+++ LearnEnglishApp/FormMenu.Designer.cs
@@ -29,12 +29,15 @@
         private void InitializeComponent()
         {
             this.button_close = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.button_removeDuplicates = new System.Windows.Forms.Button();
             this.textBox_fileName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button_close
@@ -50,23 +53,16 @@
             this.button_close.UseVisualStyleBackColor = false;
             this.button_close.Click += new System.EventHandler(this.button_close_Click);
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.button_removeDuplicates);
-            this.panel1.Location = new System.Drawing.Point(491, 44);
-            this.panel1.Name = "panel1";
-            this.panel1.Padding = new System.Windows.Forms.Padding(10);
-            this.panel1.Size = new System.Drawing.Size(300, 394);
-            this.panel1.TabIndex = 1;
-            // 
             // button_removeDuplicates
             // 
             this.button_removeDuplicates.BackColor = System.Drawing.SystemColors.ControlDark;
             this.button_removeDuplicates.Dock = System.Windows.Forms.DockStyle.Top;
             this.button_removeDuplicates.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_removeDuplicates.Location = new System.Drawing.Point(10, 10);
+            this.button_removeDuplicates.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_removeDuplicates.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_removeDuplicates.Location = new System.Drawing.Point(10, 37);
             this.button_removeDuplicates.Name = "button_removeDuplicates";
-            this.button_removeDuplicates.Size = new System.Drawing.Size(280, 50);
+            this.button_removeDuplicates.Size = new System.Drawing.Size(230, 50);
             this.button_removeDuplicates.TabIndex = 0;
             this.button_removeDuplicates.Text = "Remove duplicates";
             this.button_removeDuplicates.UseVisualStyleBackColor = false;
@@ -74,7 +70,8 @@
             // 
             // textBox_fileName
             // 
-            this.textBox_fileName.Location = new System.Drawing.Point(13, 67);
+            this.textBox_fileName.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_fileName.Location = new System.Drawing.Point(10, 60);
             this.textBox_fileName.Name = "textBox_fileName";
             this.textBox_fileName.PlaceholderText = "fileName";
             this.textBox_fileName.ReadOnly = true;
@@ -85,8 +82,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(13, 44);
+            this.label1.Location = new System.Drawing.Point(10, 37);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(124, 20);
             this.label1.TabIndex = 3;
@@ -95,13 +93,47 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(13, 8);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 28);
+            this.label2.Size = new System.Drawing.Size(73, 31);
             this.label2.TabIndex = 4;
             this.label2.Text = "Menu";
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(13, 388);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(778, 49);
+            this.panel2.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_removeDuplicates);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(541, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Size = new System.Drawing.Size(250, 338);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Actions";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox_fileName);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(13, 44);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox2.Size = new System.Drawing.Size(522, 338);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Settings";
             // 
             // FormMenu
             // 
@@ -109,16 +141,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox_fileName);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button_close);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormMenu";
             this.Padding = new System.Windows.Forms.Padding(10);
             this.Text = "FormMenu";
-            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,10 +161,12 @@
         #endregion
 
         private Button button_close;
-        private Panel panel1;
         private Button button_removeDuplicates;
         private TextBox textBox_fileName;
         private Label label1;
         private Label label2;
+        private Panel panel2;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
     }
 }
