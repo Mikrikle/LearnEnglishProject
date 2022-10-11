bf2e1dbc3e9700dabdbb2aed4a8819d1041a1704
Mihail <mihail.pe@outlook.com>
2022-10-11 10:21:35 +0300

    add checking the notify icon

--- LearnEnglishApp/FormNotify.cs
+++ LearnEnglishApp/FormNotify.cs
@@ -19,10 +19,27 @@ namespace LearnEnglishNotify
 
         public FormNotify()
         {
+            SetIcon();
             NotifyIcon.Text = "LearnEnglishApp";
-            NotifyIcon.Icon = new Icon(Path.Combine("icons", "appicon.ico"));
             NotifyIcon.ContextMenuStrip = new();
             NotifyIcon.Visible = true;
+        }
+
+        /// <summary>
+        /// Check the existence .ico and set it or warning
+        /// </summary>
+        private void SetIcon()
+        {
+            string iconPath = Path.Combine("icons", "appicon.ico");
+            if (File.Exists(iconPath))
+            {
+                NotifyIcon.Icon = new Icon(iconPath);
+            }
+            else
+            {
+                NotifyIcon.Icon = new Icon(SystemIcons.Exclamation, 40, 40);
+                MessageBox.Show($"Warning: {iconPath} not found.", "Warning");
+            }
         }
     }
 }
